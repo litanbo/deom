d4b7cbdfcac45a8185756f8b57be48c05f4d3730
litanbo <614758211@qq.com>
2022-08-29 10:15:53 +0800

    适合对比接口规范的项目

---
+++ interface-rule-fast-comparison/src/main/java/com/li/interfacerulefastcomparison/InterfaceRuleFastComparisonApplication.java
@@ -0,0 +1,13 @@
+package com.li.interfacerulefastcomparison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfaceRuleFastComparisonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterfaceRuleFastComparisonApplication.class, args);
+    }
+
+}
---
+++ interface-rule-fast-comparison/src/test/java/com/li/interfacerulefastcomparison/InterfaceRuleFastComparisonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.interfacerulefastcomparison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterfaceRuleFastComparisonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
